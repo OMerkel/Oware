822d8a631c7cd6b6d17f269369c8c8bc4bd1e010
Oliver Merkel <Merkel.Oliver@web.de>
2014-04-12 11:25:47 +0200

    Define a class of some common constants used in Oware then.

---
+++ html5/src/js/common.js
@@ -0,0 +1,20 @@
+//
+// Copyright (c) 2014 Oliver Merkel
+// All rights reserved.
+//
+// @author Oliver Merkel, <Merkel(dot)Oliver(at)web(dot)de>
+//
+
+function Common() {}
+
+Common.PLAYERNORTH = 'north';
+Common.PLAYERSOUTH = 'south';
+Common.NONE = 'none';
+Common.ACTIVE = 'active player';
+Common.BOWLS = 'bowls';
+Common.SCORE = 'score';
+
+Common.INITIALPLAYER = Common.PLAYERSOUTH;
+Common.BOWLSTOTAL = 12;
+Common.BONDUCPERBOWL = 4;
+
